18e530a20e8b701488de8cd409f55169e7758d1a
Gsus Traslaviña <140648728+gsusdavid27@users.noreply.github.com>
2023-08-30 01:35:17 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String variableValor = System.getenv("nombreVariable");
-        System.out.println("Mensaje con la variable de entorno: " + variableValor);
+        String nombreVariable = System.getenv("NOMBRE_VARIABLE");
+
+        if (nombreVariable != null) {
+            System.out.println("El valor de la variable de entorno es: " + nombreVariable);
+        } else {
+            System.out.println("La variable de entorno no está definida.");
+        }
     }
 }
