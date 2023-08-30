d1142c821ed48ccd2d99c422bff177ca63f49df6
txusdavid27 <jesus.traslavina@globant.com>
2023-08-30 00:53:07 -0500

    Done

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String variableValor = System.getenv("nombreVariable");
+        System.out.println("Mensaje con la variable de entorno: " + variableValor);
+    }
+}
